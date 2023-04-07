0e59329f0aba039bb025e5d810c13e71a1728fb9
George <GZheng8918@gmail.com>
2023-04-07 15:35:13 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const Main = document.getElementById ("main");
+Main.remove();
+
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.textContent="YOUR-NAME is the champion";
+
